56e12a96e2d3e3df2a92e05b62af2ae913372ba8
n-dps-projects <n-dps-projects@users.noreply.github.com>
2018-03-20 00:58:03 +0100

    pfioou trouv pas le bug en test random
    Signed-off-by: n-dps-projects <n-dps-projects@users.noreply.github.com>

--- 6Kyu/Python_Solves/EasyBalanceChecking.py
+++ 6Kyu/Python_Solves/EasyBalanceChecking.py
@@ -1,24 +1,26 @@
 import re
 from decimal import Decimal
 
+
 def balance(book):
     regexone = re.compile(r'[0-9]+')
     regexnum = re.compile(r'[0-9]*\.[0-9]*')
     regexstr = re.compile(r'[a-zA-Z]+')
     amount = Decimal(regexnum.match(str.split(book, '\n', 1)[0]).group())
     conteneur = str.split(book, '\n', 1)[1].split('\n')
-    retour = "Original Balance: {}\r\n".format(amount)
+    retour = "Original Balance: {0:.2f}\r\n".format(amount)
     count = 0
     totalxpense = Decimal(0.0)
     for line in conteneur:
         if line != "":
-            count = count + 1
-            firstnum = regexone.search(line).group()
-            secondstring = regexstr.search(line).group()
-            thirdnum = Decimal(regexnum.search(line).group())
-            amount -= thirdnum
-            retour += "{0} {1} {2:.2f} Balance {3:.2f}\r\n".format(firstnum, secondstring, thirdnum, amount)
-            totalxpense += thirdnum
+            if regexone.search(line).group().strip() != "" and regexstr.search(line).group().strip() != "" and regexnum.search(line).group().strip() != "":
+                firstnum = regexone.search(line).group()
+                secondstring = regexstr.search(line).group()
+                thirdnum = Decimal(regexnum.search(line).group())
+                count = count + 1
+                amount -= thirdnum
+                retour += "{0} {1} {2:.2f} Balance {3:.2f}\r\n".format(firstnum, secondstring, thirdnum, amount)
+                totalxpense += thirdnum
     average = totalxpense / count
     retour += "Total expense  {0:.2f}\r\n".format(totalxpense)
     retour += "Average expense  {0:.2f}".format(average)
